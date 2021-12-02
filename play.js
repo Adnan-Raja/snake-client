0402a26518d6a8dd49a166158d8c9ad8566cfec3
Adnan-Raja <adnan.saeed.raja@gmail.com>
2021-12-01 22:43:58 -0500

    update

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
-const net = require("net");
 const connect = require('./client.js');
+const setupInput = require("./input.js");
 
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.setEncoding("utf8");
-    stdin.on("data", (data) => {
-       if ( data === '\u0003' ) {
-         process.exit();
-       }
-     });
-  return stdin;
-};
 
 console.log("Connecting ...");
 connect();
+
 setupInput();
